154b3d306f4ec90fccf253efe1e92fc60467ddf1
chalabaj <chalabaj@vscht.cz>
2017-12-05 10:57:38 +0100

    CP2k toolchain Salomon launcher

--- instaconf_cp2k.sh
+++ instaconf_cp2k.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 module load imkl/2017.1.132-iimpi-2017a
+module load MPICH/3.2-GCC-5.3.1-snapshot-20160419-2.25
 
 #export MKLROOT
-./install_cp2k_toolchain.sh --install-all --mpi-mode=mpich --math-mode=mkl --enable-omp=no  --with-binutils=system
+./install_cp2k_toolchain.sh --install-all --mpi-mode=mpich --math-mode=mkl --enable-omp=no --with-binutils=system --with-mkl=system --with-gcc=system --with-mpich=system --with-binutils=system --with-make=system --with-cmake=system
