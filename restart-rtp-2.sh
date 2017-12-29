7028a70304e3560c7acf0d8fc4f66a59af1b00eb
chalabaj <chalabaj@vscht.cz>
2017-12-29 10:58:58 +0100

    restart 2 RTP CP2K dyn

---
+++ restart-rtp-2.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+# restart CP2K multiple trajectory RTP dynamics
+# requires previour RTP run
+# launch this in supdirectory containing TRAJ.X folders
+folder=frankPBE        # Name of the folder with trajectories
+ntrajs=47              # number of trajectories 
+projectname=DIMER      # projectname
+TASKPERJOB=3           # How many simulataneous tasks will be executed within one qsub file
+NJOBS=16               # Number of submited files
+NPROCS=8               # $CPUs per mpirun -np switch
+#QSUB PBS SETTINGS------------------
+queue=qlong             # Avaible ques: qprod qlong qexp qfree
+walltime=144:00:00      # The maximum runtime in qlong is 144 hours, qprod is 48 hours,  qexp is 1 hour
+PATCHED=unpatched
+runtype=RTP_RESTART_2   # dont set to -SCT, WF or RTP- cause rm procedure remove already  finished runs
+INPUTDIR=inputsdir-pbe
+onlysub=sub   # all or just sub"
+IREST=2  #i-th restart / second, first....
+cd $folder
+TRAJSDIR=$PWD
+echo "Starting work in $TRAJSDIR"
+
+#preparing simulataneous submitable script
+for ((k=1;k<=$NJOBS;k++))
+do
+
+cat > $projectname.$runtype.$k.sh << EOF
+#!/bin/bash
+#PBS -q $queue
+#PBS -l select=1:ncpus=24:mpiprocs=24:mem=96gb
+#PBS -l walltime=$walltime
+#PBS -A OPEN-11-34
+#PBS -e ERROR_$projectname.$runtype.$k.sh
+
+EOF
+done
+
+k=1      # .sh script
+ITRAJ=1  #TRAJ
+
+for ((ITRAJ=1;ITRAJ<=$ntrajs;ITRAJ++))
+do
+cd TRAJ.$ITRAJ/RTP_RESTART_1
+echo "Working in:  $PWD"
+logfile=$(ls *log | head -1)
+SCRDIR=$(head -3 $logfile | tail -1)
+KDE=$(head -4 $logfile  | tail -1)
+
+echo "Logfile: $logfile"
+echo "Final folder: $KDE"
+#echo "Copy done in TRAJ.$k---------------------------------"
+
+#cd $SCRDIR
+#echo "Checking for latest  restart file in $PWD"
+
+#this depends how far did the trajectories got in the last RTP run, check tail of eners
+#for STEP in {70000..120000..500}
+ #do 
+ #  if [[ -e "DIMER_$ITRAJ-RESTART-1_$STEP.rtpwfn" ]]; then
+ #  echo "File DIMER_$ITRAJ-RESTART-1_$STEP.rtpwfn EXISTS"
+ #   latest=$STEP
+ #  fi
+    
+#done
+#echo "Last restart is: $latest"
+#copy only necessary files
+# file name depends on projectname in launchCP2ks-dynamics
+cp  $SCRDIR/${projectname}_$ITRAJ-pos-1.xyz                 $KDE
+cp  $SCRDIR/${projectname}_$ITRAJ-1.ener                    $KDE
+#cp  $SCRDIR/${projectname}_$ITRAJ-RESTART-1_$latest.rtpwfn  $KDE
+#cp  $SCRDIR/${projectname}_$ITRAJ-1_$latest.restart         $KDE
+cp  $SCRDIR/${projectname}_$ITRAJ-1.restart                 $KDE
+cp  $SCRDIR/${projectname}_$ITRAJ-RESTART.rtpwfn            $KDE
+cp  $SCRDIR/*.bak*                                 $KDE
+cp  $SCRDIR/*.out                                  $KDE
+
+cd $TRAJSDIR/TRAJ.$ITRAJ/
+#echo "Removing old SCRATCH DIR"
+#rm -r $SCRDIR
+echo "Working now in $PWD." 
+#echo "Creating dir and cp file for restart containing:"
+
+if [[ -d "$runtype" ]]; then
+rm -r $runtype; mkdir $runtype
+else
+mkdir $runtype
+fi
+#!!!!!!!!!!!!CHANGE FOR NEXT RESTART HERE 
+cp  RTP_RESTART_1/${projectname}_$ITRAJ-pos-1.xyz                RTP_RESTART_2/.
+cp  RTP_RESTART_1/${projectname}_$ITRAJ-1.ener                   RTP_RESTART_2/.
+cp  RTP_RESTART_1/${projectname}_$ITRAJ-RESTART.rtpwfn           RTP_RESTART_2/.
+cp  RTP_RESTART_1/${projectname}_$ITRAJ-1.restart                RTP_RESTART_2/$runtype.inp
+# RT RESTART folder is almost ready
+
+
+cd ..  #!!!!!! now we are in folder with TRAJS
+
+#PREPARE file which gonna be proccesed after submition
+cat > TRAJ.$ITRAJ/$runtype/r.$runtype.$ITRAJ << EOF
+#!/bin/bash
+#PBS -V
+
+# where to copy final data
+IWORKDIR=$PWD/TRAJ.$ITRAJ/$runtype    
+JOBNAME=${folder}_${runtype}_traj${ITRAJ}_\${PBS_JOBID}
+INPFILE=$runtype.inp
+WFFILE=${projectname}_${ITRAJ}-RESTART.rtpwfn
+NPROCS=${NPROCS}   # ech paralell job to have X cores
+LOGFILE=${folder}_${runtype}_${ITRAJ}_\${PBS_JOBID}.log #input file
+PATCHED=$PATCHED                                                         #number of CPUs
+OUTFILE=$runtype.out
+RUNTYPE=${runtype}
+
+EOF
+cat ../$INPUTDIR/CP2Ks-dynamics_rtp_restart >> TRAJ.$ITRAJ/$runtype/r.$runtype.$ITRAJ
+chmod +x TRAJ.$ITRAJ/$runtype/r.$runtype.$ITRAJ
+
+#Simultaneous jobs in one file 
+echo "(cd $TRAJSDIR/TRAJ.$ITRAJ/$runtype/; ./r.$runtype.$ITRAJ ) &">> $TRAJSDIR/$projectname.$runtype.$k.sh
+
+
+if [[ `expr $ITRAJ % $TASKPERJOB` -eq 0 ]]; then
+ let k++
+fi
+
+done
+
+echo "DONE WITH PREPARING RESTART FOLDERS. Waiting."
+
+for ((i=1;i<=$ntrajs;i++))
+do
+ # sed -i "s/COORD_FILE_NAME ${projectname}_${i}.xyz/#COORD_FILE_NAME ${projectname}_${i}.xyz/g" TRAJ.$i/$runtype/$runtype.inp
+ # sed -i "s/COORD_FILE_FORMAT  XYZ/#COORD_FILE_FORMAT  XYZ/g" TRAJ.$i/$runtype/$runtype.inp
+  sed -i "s/NUMBER_OF_ATOMS  6/#NUMBER_OF_ATOMS  6/g" TRAJ.$i/$runtype/$runtype.inp
+  sed -i "s/MULTIPLE_UNIT_CELL  1 1 1/#MULTIPLE_UNIT_CELL  1 1 1/g" TRAJ.$i/$runtype/$runtype.inp
+  sed -i "s/MAX_ITER  5/MAX_ITER  25/g" TRAJ.$i/$runtype/$runtype.inp
+done
+
+
+cd $TRAJSDIR
+k=1
+for ((k=1;k<=$NJOBS;k++))
+do
+   if [[ -f "$projectname.$runtype.$k.sh" ]];then
+      echo "wait" >> $projectname.$runtype.$k.sh     # whille all parallel jobs running, wait until the very last traj is done
+      echo "Submiting: $projectname.$runtype.$k.sh"
+      chmod +x $projectname.$runtype.$k.sh
+      qsub $projectname.$runtype.$k.sh
+   fi
+
+done
+
